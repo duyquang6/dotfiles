5e3bcbd5ae00c98d39a4c30723c9ad170dcecb39
duyquang6 <nguyenduyquang06@gmail.com>
2024-05-09 07:28:20 +0700

    feat: use lvim

--- linux.sh
+++ linux.sh
@@ -1,2 +1,6 @@
-ln -s $(pwd)/nvim/init.vim ~/.config/nvim/init.vim
+# setup
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# ln -s $(pwd)/nvim/init.vim ~/.config/nvim/init.vim
 ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+ln -s $(pwd)/lvim/config.lua ~/.config/lvim/config.lua
