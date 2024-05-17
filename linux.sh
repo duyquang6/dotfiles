edf898d75c9d6858ad0f588079dd34cac39e66e0
duyquang6 <nguyenduyquang06@gmail.com>
2024-05-17 23:43:50 +0700

    update config lazyvim

--- linux.sh
+++ linux.sh
@@ -3,7 +3,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s $(pwd)/.tmux.conf ~/.tmux.conf
 
 # setup vim
-# ln -s $(pwd)/nvim/init.vim ~/.config/nvim/init.vim
+ln -s $(pwd)/nvim ~/.config/nvim
 ln -s $(pwd)/lvim/config.lua ~/.config/lvim/config.lua
 ln -s $(pwd)/lvim/lua ~/.config/lvim/lua
 ln -s $(pwd)/lvim/lsp-settings ~/.config/lvim/lsp-settings
