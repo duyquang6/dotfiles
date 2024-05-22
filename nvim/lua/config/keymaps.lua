260b011659fb4096b3434748352dfce3f32debcb
duyquang6 <nguyenduyquang06@gmail.com>
2024-05-22 07:21:40 +0700

    update keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -40,19 +40,19 @@ if hop_ok then
   local modes = { "n", "o", "x" }
   local opts = { remap = true, silent = true }
   keymap(modes, "f", function()
-    hop.hint_char1({ direction = directions.AFTER_CURSOR })
+    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
   end, opts)
 
   keymap(modes, "F", function()
-    hop.hint_char1({ direction = directions.BEFORE_CURSOR })
+    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
   end, opts)
 
   keymap(modes, "t", function()
-    hop.hint_char1({ direction = directions.AFTER_CURSOR, hint_offset = -1 })
+    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
   end, opts)
 
   keymap(modes, "T", function()
-    hop.hint_char1({ direction = directions.BEFORE_CURSOR, hint_offset = 1 })
+    hop.hint_char1({ direction = directions.BEFORE_CURSOR, hint_offset = 1, current_line_only = true })
   end, opts)
 
   nkeymap("s", function()
