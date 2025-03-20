56005736e2f58013f65f7a720258207fa3ff71a7
ligt <me@ligt.dev>
2025-03-20 07:52:34 +0700

    update daily

--- scripts/power-off.sh
+++ scripts/power-off.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 yad --title "Power Options" \
-  --text "<span font=\"15\">Power off or reboot now?</span>\n" \
+  --text "<span font=\"15\">Time to farewell ?</span>\n" \
   --borders "30" \
   --text-align "center" \
   --center \
   --fixed \
   --button "Cancel":1 \
   --button "Reboot":2 \
+  --button "Suspend":3 \
   --button "Power Off":0
 
 case $? in
 0) systemctl poweroff ;; # Power off when "Power Off" (exit code 0) is clicked
 2) systemctl reboot ;;   # Reboot when "Reboot" (exit code 2) is clicked
-1) exit 0 ;;             # Exit when "Cancel" (exit code 1) is clicked
+3) systemctl suspend ;;
+1) exit 0 ;; # Exit when "Cancel" (exit code 1) is clicked
 esac
